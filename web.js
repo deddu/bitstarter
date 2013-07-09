e20211ac60f46e46089fa2637da71fb11f7a935f
Ubuntu <ubuntu@ip-172-31-17-1.us-west-2.compute.internal>
2013-07-09 16:35:58 +0000

    next

--- web.js
+++ web.js
@@ -4,14 +4,29 @@ var index=('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile("./index.html", function read(err,data){
-if (err ){throw err;}content=data;
-	response.send(content);
+fs.readFile("./index.html", function read(err,data){
+if (err ){
+	throw err;
+	response.writeHead(400, {"Content-Type":"text/plain"});
+	response.write("index not found");
+	response.end();
+}
+content=data;
+response.writeHead(200,{"Content-Type":"text/html"});
+response.write(content);
+response.end();
+//console.log(content);
 });
+
+
  // response.send('Hello Boss!\n welcome back');
 });
 
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+  //readfile()
   console.log("Listening on " + port);
 });
